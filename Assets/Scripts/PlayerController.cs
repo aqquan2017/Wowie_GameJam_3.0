5516fb2ac84d16130cf09d8147d248b0f29dca81
HungOishiro <63391108+HungOishiro@users.noreply.github.com>
2021-02-27 02:36:11 +0700

    Player and cam movement, shoot updated

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 playerDir;
+    private Rigidbody2D playerRb;
+    public float moveSpeed = 5;
+
+    private Vector2 mousePos;
+    public Camera cam;
+
+    public GameObject bulletPrefabs;
+    public Transform bulletStartPoint;
+    private float bulletMoveSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerDir.x = Input.GetAxisRaw("Horizontal");
+        playerDir.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        Shooting();
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.MovePosition(playerRb.position + playerDir * moveSpeed * Time.deltaTime);
+
+        Vector2 lookDir = mousePos - (Vector2)this.transform.position;
+        float lookAngle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90;
+
+        playerRb.rotation = lookAngle;
+    }
+
+    void Shooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefabs, bulletStartPoint.position, this.transform.rotation);
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+            bulletRb.AddForce(bullet.transform.up * bulletMoveSpeed, ForceMode2D.Impulse);
+        }
+    }
+}
