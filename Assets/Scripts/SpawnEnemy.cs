ac27b9ab44aae9d0ced65aebebb93fd260df8c5d
HungOishiro <63391108+HungOishiro@users.noreply.github.com>
2021-02-27 16:21:11 +0700

    Spawn Enemy, Prefabs Player and Cam Upadated

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject enemyPrefabs;
+    private float timeToSpawn = 5;
+    public float timeRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeRemaining += Time.deltaTime;
+        if (timeRemaining >= timeToSpawn)
+        {
+            GameObject enemy = Instantiate(enemyPrefabs, this.transform.position, Quaternion.identity);
+            timeRemaining = 0;
+        }
+    }
+
+}
